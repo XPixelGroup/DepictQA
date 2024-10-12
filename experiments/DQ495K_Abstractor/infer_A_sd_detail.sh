66c7719ea8d97920e48cb5dbc065fbf9aead79e7
zhiyuanyou <zhiyuanyou@foxmail.com>
2024-10-12 10:08:44 +0800

    add abstractor for 1024x2048 imgs

---
+++ experiments/DQ495K_Abstractor/infer_A_sd_detail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+src_dir=../../src/
+export PYTHONPATH=$src_dir:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=$1
+
+python $src_dir/infer.py \
+    --meta_path KADIS700K_s384/A_sd_detail/metas/test_A_sd_detail_200.json \
+    --dataset_name kadis_A_sd_detail \
+    --task_name quality_single_A_noref \
+    --batch_size 16 \
---
+++ experiments/DQ495K_Abstractor/infer_refAB_bapps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+src_dir=../../src/
+export PYTHONPATH=$src_dir:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=$1
+
+python $src_dir/infer.py \
+    --meta_path BAPPS/metas/test_refAB_single_bapps_9.4k.json \
+    --dataset_name bapps_refAB \
+    --task_name quality_compare \
+    --batch_size 16 \
